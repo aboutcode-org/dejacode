c82953469b837943ace2c03d7a4fe986d1798267
tdruez <tdruez@nexb.com>
2024-08-22 07:52:18 +0400

    Update URL in notice headers #158
    Signed-off-by: tdruez <tdruez@nexb.com>

--- reporting/static/js/change_columntemplate.js
+++ reporting/static/js/change_columntemplate.js
@@ -3,7 +3,7 @@
 # Copyright (c) nexB Inc. and others. All rights reserved.
 # DejaCode is a trademark of nexB Inc.
 # SPDX-License-Identifier: AGPL-3.0-only
-# See https://github.com/nexB/dejacode for support or download.
+# See https://github.com/aboutcode-org/dejacode for support or download.
 # See https://aboutcode.org for more information about AboutCode FOSS projects.
 #
 */
