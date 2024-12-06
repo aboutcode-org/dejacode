3ee68960aed8d273662a502db8e00d8f04013ed6
tdruez <tdruez@nexb.com>
2024-12-06 13:57:45 +0400

    Add affected_packages and affected_products in vulnerabilities endpoints #104
    Signed-off-by: tdruez <tdruez@nexb.com>

--- vulnerabilities/api.py
+++ vulnerabilities/api.py
@@ -7,8 +7,12 @@
 #
 
 
-from rest_framework import viewsets
+from django.db.models import Prefetch
 
+from rest_framework import viewsets
+from rest_framework import serializers
+
+from component_catalog.models import Package
 from dje.api import DataspacedAPIFilterSet
 from dje.api import DataspacedSerializer
 from dje.api import ExtraPermissionsViewSetMixin
@@ -18,9 +22,29 @@ from dje.filters import MultipleUUIDFilter
 from vulnerabilities.filters import RISK_SCORE_RANGES
 from vulnerabilities.filters import ScoreRangeFilter
 from vulnerabilities.models import Vulnerability
+from product_portfolio.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    display_name = serializers.ReadOnlyField(source="__str__")
+
+    class Meta:
+        model = Product
+        fields = (
+            "display_name",
+            "api_url",
+        )
+        extra_kwargs = {
+            "api_url": {
+                "view_name": "api_v2:product-detail",
+                "lookup_field": "uuid",
+            },
+        }
 
 
 class VulnerabilitySerializer(DataspacedSerializer):
+    affected_products = serializers.SerializerMethodField()
+
     class Meta:
         model = Vulnerability
         fields = (
@@ -34,13 +58,27 @@ class VulnerabilitySerializer(DataspacedSerializer):
             "exploitability",
             "weighted_severity",
             "risk_score",
+            "affected_packages",
+            "affected_products",
         )
         extra_kwargs = {
             "api_url": {
                 "view_name": "api_v2:vulnerability-detail",
                 "lookup_field": "uuid",
             },
+            "affected_packages": {
+                "view_name": "api_v2:package-detail",
+                "lookup_field": "uuid",
+            },
         }
+
+    def get_affected_products(self, obj):
+        products = (
+            product_package.product
+            for package in obj.affected_packages.all()
+            for product_package in package.productpackages.all()
+        )
+        return ProductSerializer(products, many=True, context=self.context).data
 
 
 class VulnerabilityFilterSet(DataspacedAPIFilterSet):
@@ -77,4 +115,15 @@ class VulnerabilityViewSet(ExtraPermissionsViewSetMixin, viewsets.ReadOnlyModelV
     )
 
     def get_queryset(self):
-        return super().get_queryset().scope(self.request.user.dataspace).order_by_risk()
+        package_qs = Package.objects.only("uuid", "dataspace__name")
+
+        return (
+            super()
+            .get_queryset()
+            .scope(self.request.user.dataspace)
+            .prefetch_related(
+                Prefetch("affected_packages", queryset=package_qs),
+                Prefetch('affected_packages__productpackages__product'),
+            )
+            .order_by_risk()
+        )
