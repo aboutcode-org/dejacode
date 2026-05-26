a4f1ae23b5a31c5759c6107187ae8c85cb97c48a
tdruez <tdruez@aboutcode.org>
2026-05-26 09:05:37 +0400

    fix unit test
    Signed-off-by: tdruez <tdruez@aboutcode.org>

--- product_portfolio/tests/test_views.py
+++ product_portfolio/tests/test_views.py
@@ -455,6 +455,13 @@ class ProductPortfolioViewsTestCase(MaxQueryMixin, TestCase):
         response = self.client.get(url)
         self.assertContains(response, modal_id)
         self.assertContains(response, modal_js)
+        self.assertContains(response, "csrf_header.js")
+
+        # Ensure the CSRF script is present even if product is locked
+        locked_status = make_product_status(self.dataspace)
+        self.product1.update(configuration_status=locked_status)
+        response = self.client.get(url)
+        self.assertContains(response, "csrf_header.js")
 
     @mock.patch("dejacode_toolkit.vulnerablecode.VulnerableCode.is_configured")
     def test_product_portfolio_detail_view_tab_vulnerability_label(self, mock_is_configured):
@@ -1161,7 +1168,6 @@ class ProductPortfolioViewsTestCase(MaxQueryMixin, TestCase):
             "This product version is marked as read-only, preventing any modifications "
             "to its inventory."
         )
-        expected4 = "csrf_header.js"
 
         locked_status = make_product_status(self.dataspace, is_locked=True)
         self.product1.update(configuration_status=locked_status)
@@ -1169,7 +1175,6 @@ class ProductPortfolioViewsTestCase(MaxQueryMixin, TestCase):
         self.assertContains(response, expected1, html=True)
         self.assertContains(response, expected2, html=True)
         self.assertContains(response, expected3, html=True)
-        self.assertContains(response, expected4, html=True)
 
     def test_product_portfolio_list_view_secured_queryset(self):
         self.client.login(username=self.basic_user.username, password="secret")
