3297d2d73ebe8f0a679a210be262e4eb8fd2f572
tdruez <tdruez@aboutcode.org>
2026-06-30 18:20:59 +0400

    add aboutcode app for notifications
    Signed-off-by: tdruez <tdruez@aboutcode.org>

---
+++ aboutcode/notification/__init__.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# DejaCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: AGPL-3.0-only
+# See https://github.com/aboutcode-org/dejacode for support or download.
+# See https://aboutcode.org for more information about AboutCode FOSS projects.
+#
+
+from aboutcode.notification.models import AbstractWebhookSubscription
+from aboutcode.notification.models import AbstractWebhookDelivery
+
+__version__ = "0.1.0"
+
+__all__ = ["AbstractWebhookSubscription", "AbstractWebhookDelivery"]
